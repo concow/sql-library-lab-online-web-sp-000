1ccab70e39782d06f3d81fed09e4a4a290723fa1
Connor Cowan <ccowan003@comcast.net>
2019-09-04 16:08:07 -0700

    Done.

--- lib/data.sql
+++ lib/data.sql
@@ -13,3 +13,12 @@ INSERT INTO characters (id, name, motto, species, author_id) VALUES (5, "Charact
 INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 1, 4);
 
 INSERT INTO character_books (id, book_id, character_id) VALUES (9, 4, 5), (10, 4, 6), (11, 5, 6), (12, 6, 6), (13, 4, 7), (14, 5, 7), (15, 6, 7), (16, 4, 8);
+
+INSERT INTO Characters (name, motto, species, author_id) VALUES ("Jon Snow", "motto", "human", 1);
+INSERT INTO Characters (name, motto, species, author_id) VALUES ("Daenerys Targaryen", "motto", "human", 1;
+INSERT INTO Characters (name, motto, species, author_id) VALUES ("Arya Stark", "motto", "human", 1;
+INSERT INTO Characters (name, motto, species, author_id) VALUES ("Drogon", "motto", "dragon", 1);
+INSERT INTO Characters (name, motto, species, author_id) VALUES ("Eddard Stark", "Winter is Coming", "human", 1);
+INSERT INTO Characters (name, motto, species, author_id) VALUES ("Frodo Baggins", "motto", "hobbit", 2);
+INSERT INTO Characters (name, motto, species, author_id) VALUES ("Gandalf", "Fly you fools!", "wizard", 2);
+INSERT INTO Characters (name, motto, species, author_id) VALUES ("Samwise Gamgee" "motto", "hobbit", 2);
---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO Series (title, author_id, subgenre_id) VALUES ("A Song of Ice and Fire", 1, 1);
+
+INSERT INTO Series (title, author_id, subgenre_id) VALUES ("Lord of the Rings", 2, 2);
+
+INSERT INTO Books (title, year, series_id) VALUES ("A Game of Thrones", 1996, 1);
+
+INSERT INTO Books (title, year, series_id) VALUES ("A Clash of Kings", 1998, 1);
+
+INSERT INTO Books (title, year, series_id) VALUES ("A Storm of Swords", 2000, 1);
+
+INSERT INTO Books (title, year, series_id) VALUES ("The Fellowship of the Ring", 1954, 2);
+
+INSERT INTO Books (title, year, series_id) VALUES ("The Two Towers", 1955, 2);
+
+INSERT INTO Books (title, year, series_id) VALUES ("The Return of the King", 1955, 2);
